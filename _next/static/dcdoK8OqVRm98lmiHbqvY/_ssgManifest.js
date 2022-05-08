ee69753ce29dbd16b72a7f78ee175950f0f7310a
ShotaroKataoka <42331656+ShotaroKataoka@users.noreply.github.com>
2022-05-08 03:25:14 +0000

    Update build to output generated at 63fce2a

---
+++ _next/static/dcdoK8OqVRm98lmiHbqvY/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Farticle\u002Flist","\u002Farticle\u002Ftags","\u002F","\u002Fprofile","\u002Fworks","\u002Farticle\u002F[slug]","\u002Farticle\u002Ftags\u002F[tags]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
